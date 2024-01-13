2fa336727513ab49b87fc7001ba925ecc12eca1b
tofekkhan <tofeequekhan87@gmail.com>
2024-01-13 16:16:39 -0600

    Leet code

---
+++ src/main/java/com/tofek/example/spring/demo/leetcode/Count_Asterisks_2315.java
@@ -0,0 +1,36 @@
+package com.tofek.example.spring.demo.leetcode;
+
+import org.springframework.util.StringUtils;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Count_Asterisks_2315 {
+
+    public static void main(String[] args) {
+        countAsterisks();
+    }
+
+    public static void countAsterisks(){
+        String s = "l|*e*et|c**o|*de|";
+        int c = StringUtils.countOccurrencesOf(s, "|");
+        String[] str = s.split("\\|");
+        int count = 0;
+        for(int i=0; i<str.length;){
+            count = count+countGivenString(str[i], "*");
+            i = i+2;
+        }
+        System.out.println(count);
+    }
+
+    private static int countGivenString(String s, String c) {
+        int ccc = 0;
+        char[] charString = s.toCharArray();
+        for(int i = 0; i<charString.length; i++){
+            if(c.equalsIgnoreCase(String.valueOf(charString[i]))){
+                ccc++;
+            }
+        }
+        return ccc;
+    }
+}
---
+++ src/main/java/com/tofek/example/spring/demo/leetcode/MaxNestingDepthParentheses_1614.java
@@ -0,0 +1,18 @@
+package com.tofek.example.spring.demo.leetcode;
+
+public class MaxNestingDepthParentheses_1614 {
+
+    public static void main(String[] args) {
+        Maximum_Nesting_Depth_Parentheses();
+    }
+
+    public static void Maximum_Nesting_Depth_Parentheses(){
+        String input = "";
+        for(int i=0; i<input.toCharArray().length; i++){
+            if("(".equalsIgnoreCase(String.valueOf(input.toCharArray()[i]))){
+
+            }
+        }
+
+    }
+}
